6116cce304f2712a625e6e4e39d923ab0a125f28
akroutj <akrout.julian@gmail.com>
2022-02-07 16:20:09 +0100

    Mise en place structure cards accueil

--- front/js/script.js
+++ front/js/script.js
@@ -15,20 +15,27 @@ getProducts();
 function addProducts(data) {
     for (let curr of data) {
         const linkProduct = makeLink(curr._id)
-        appendChildren(linkProduct)
-        makeArticle()
+        const articleProduct = makeArticle()
+        const image = makeImage()
+        const title = makeTitle()
+        const paragraph = makeParagraph()
 
+
+        appendChildren(linkProduct)
+        linkProduct.appendChild(articleProduct)
+        articleProduct.appendChild(image)
+        articleProduct.appendChild(title)
+        articleProduct.appendChild(paragraph)
     }
 }
 
 function makeLink(id) {
     const linkProduct = document.createElement("a")
     linkProduct.href = "./product.html?id=" + id
-    linkProduct.textContent = "Canapé"
     return linkProduct
 }
 
-function appendChildren(linkProduct, article) {
+function appendChildren(linkProduct) {
     const items = document.querySelector("#items")
     items.appendChild(linkProduct)
 
@@ -36,10 +43,26 @@ function appendChildren(linkProduct, article) {
 
 function makeArticle() {
     const article = document.createElement("article")
-    article.textContent = "Coin coin"
-    items.appendChild(article)
     return article
 }
+
+function makeImage() {
+    const image = document.createElement("img")
+    return image
+}
+
+function makeTitle(h3) {
+    const title = document.createElement("h3")
+    title.textContent = "TITRE"
+    return title
+}
+
+function makeParagraph(p) {
+    const paragraph = document.createElement("p")
+    paragraph.textContent = "paragraph"
+    return paragraph
+}
+
 
 
 
